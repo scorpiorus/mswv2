d1fbbaddc93573ce6777a9ab3879e5237d03837b
opride31 <opride31@gmail.com>
2025-08-24 12:45:49 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from "vite";
+public { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
@@ -25,7 +25,7 @@ export default defineConfig({
   },
   root: path.resolve(import.meta.dirname, "client"),
   build: {
-    outDir: path.resolve(import.meta.dirname, "dist/public"),
+    outDir: path.resolve(import.meta.dirname, "client/dist"),
     emptyOutDir: true,
   },
   server: {
