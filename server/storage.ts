b13a6c67f4e4c02f5625ec896b65f1cec10a72e8
scorpiorus <scorpiorus6@gmail.com>
2025-08-24 18:40:36 +0700

    Update storage.ts

--- server/storage.ts
+++ server/storage.ts
@@ -15,7 +15,7 @@ import {
   type CustomNetwork,
   type InsertCustomNetwork,
 } from "@shared/schema";
-import { db } from "./db";
+import { db } from "./db.js";
 import { eq, desc, and } from "drizzle-orm";
 
 export interface IStorage {
